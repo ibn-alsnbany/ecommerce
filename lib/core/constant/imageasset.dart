fbda5e8f905ce1948939d4f78717b598b8c54a3a
mohammed <ibn.alsnbany@gmail.com>
2025-07-26 22:19:43 +0300

    make root images

---
+++ lib/core/constant/imageasset.dart
@@ -0,0 +1,8 @@
+class Imageasset {
+  static const String rootImages = "assets/images";
+
+  static const String onBoradingImageOne = "$rootImages/onboardingone.PNG";
+  static const String onBoradingImageTwo = "$rootImages/onboardingtwo.PNG";
+  static const String onBoradingImageThree = "$rootImages/onboardingthree.PNG";
+  static const String onBoradingImageFour = "$rootImages/onboardingfour.PNG";
+}
