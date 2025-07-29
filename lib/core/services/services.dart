4d478e12f61a60e868c6918d8f3ab0a82f434b3a
mohammed <ibn.alsnbany@gmail.com>
2025-07-29 22:12:17 +0300

    start localazition

---
+++ lib/core/services/services.dart
@@ -0,0 +1,6 @@
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MyServices extends GetxService {
+  late SharedPreferences sharedPreferences;
+}
